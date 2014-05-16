3325e18ecbd8400714b5f11720ad30e1af930339
enisaac <isaacs_ethan@yahoo.com>
2014-05-15 22:19:36 -0700

    Update Helloworld.py.py

--- Helloworld.py.py
+++ Helloworld.py.py
@@ -3,7 +3,7 @@ myname = input ("What is your name?")
 print (myname)
 if(myname == "John"):
     print ("I was trying to do this before I learned how for thirty minutes. I'm ahead of the game. I could have saved forty minutes, if I just watched the next tutorial")
-elif (myname == "Ethan")
+elif (myname == "Ethan"):
     print ("You wrote this!")
 else:
     print ("If you are not John leave now.")
